430fe1314e58b37650081c869cca4ff900ac7feb
jt-owo <joniwalkerlp@gmail.com>
2020-10-05 15:29:39 +0200

    Add Layout

---
+++ Tsumugi/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tsumugi.Controllers
+{
+    public class BaseController : Controller
+    {
+    }
+}
---
+++ Tsumugi/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tsumugi.Controllers
+{
+    public class DashboardController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
